948d6825e8a60185e6efac6d4499e0d5f0ef53c4
NewBean0312 <newbean0312@gmail.com>
2024-05-17 10:00:45 +0900

    미디어쿼리 PC용 css 작업

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d253298.0148028186!2d128.27176736978325!3d38.063551830922755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35622de887f205d7%3A0x388aeb08e26897f9!2z6rCV7JuQ64-EIO2Pieywveq1sA!5e0!3m2!1sko!2skr!4v1715905342653!5m2!1sko!2skr"
+        src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3158.2286079692144!2d128.70440142813803!3d37.66733574366271!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzfCsDQwJzAyLjQiTiAxMjjCsDQyJzIxLjIiRQ!5e0!3m2!1sko!2skr!4v1715907378648!5m2!1sko!2skr"
         style={{ border: "0" }}
         allowFullScreen={true}
       ></iframe>
